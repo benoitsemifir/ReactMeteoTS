82b232a591a30d243d71732e5db651c6503a3ff4
Benoit <benoit.routier@semifir.com>
2021-05-03 22:42:53 +0200

    cleanup and documentation

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -1,23 +1,36 @@
-// import LocationService from "./LocationService";
-
 const url = 'https://api.openweathermap.org/data/2.5/weather'
 const apiKey = 'c06516a3bb199bb320181c589f72433c'
 
 let HttpHeaders = new Headers();
 
-var init: RequestInit = { method: 'GET',
-            headers: HttpHeaders,
-            mode: 'cors',
-            cache: 'default' };
-        
+var init: RequestInit = {
+    method: 'GET',
+    headers: HttpHeaders,
+    mode: 'cors',
+    cache: 'default'
+};
+/**
+ * Simple service to get weahter forcast from OpenWeatherAPI
+ */
 class WeatherService {
 
-    async getWeatherByCity(city: string){
-        return await fetch(`${url}?q=${city}&appid=${apiKey}&lang=fr&units=metric`,init).then(resp=>resp.json());
+    /**
+     * Get the weather of a city by it's name
+     * @param city Where to get the forecast for
+     * @returns Response of API
+     */
+    async getWeatherByCity(city: string) {
+        return await fetch(`${url}?q=${city}&appid=${apiKey}&lang=fr&units=metric`, init).then(resp => resp.json());
     }
 
-    async getWeatherByLocation(lat: string, lon: string){
-        return await fetch(`${url}?lat=${lat}&lon=${lon}&appid=${apiKey}&lang=fr&units=metric`,init).then(resp=>resp.json())
+    /**
+     * Get the weather of current location (using HTML5 navigator)
+     * @param lat latitide
+     * @param lon longitude
+     * @returns Response of API
+     */
+    async getWeatherByLocation(lat: string, lon: string) {
+        return await fetch(`${url}?lat=${lat}&lon=${lon}&appid=${apiKey}&lang=fr&units=metric`, init).then(resp => resp.json())
     }
 
 }
